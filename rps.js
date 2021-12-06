beaa2001d3ce4078df5aa42966d6f1a74ea17606
Saif <saifaali732@gmail.com>
2021-12-06 14:43:28 -0500

    completed console implementation

--- rps.js
+++ rps.js
@@ -11,3 +11,48 @@ function computerPlay() {
             return "scissors"
     }
 }
+
+//Plays one round of rps
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection == "scissors" ||
+        playerSelection === "paper" && computerSelection == "rock" ||
+        playerSelection === "scissors" && computerSelection == "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection === "rock" && playerSelection == "scissors" ||
+        computerSelection === "paper" && playerSelection == "rock" ||
+        computerSelection === "scissors" && playerSelection == "paper") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return 'Tie!';
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    //play 5 rounds, keeping track of the score
+    for (let i = 0; i < 5; i++) {
+        const conputerSelection = computerPlay();
+        const playerSelection = prompt("rock, paper, or scissors?");
+        const result = playRound(playerSelection, conputerSelection);
+        //update score if someone won the round
+        if (result.charAt(4) === 'w') {
+            playerScore++;
+        } else if (result.charAt(4) === 'l') {
+            computerScore++;
+        }
+        console.log(result);
+        if (i != 4) {
+            console.log(`Current Score is You: ${playerScore} Computer: ${computerScore}`);
+        }
+    }
+    //report final scores and winner
+    if (playerScore > computerScore) {
+        console.log(`You Win! Final score: ${playerScore} - ${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You Lose! Final score: ${playerScore} - ${computerScore}`);
+    } else {
+        console.log(`You Tied! Final score: ${playerScore} - ${computerScore}`);
+    }
+}
