45425a244da560360f0a1209c78bf5f235c82c7b
Saif <saifaali732@gmail.com>
2021-12-06 03:15:38 -0500

    changed return value of computer

--- rps.js
+++ rps.js
@@ -4,10 +4,10 @@ function computerPlay() {
     let choice = Math.floor(Math.random() * 3) + 1;
     switch (choice) {
         case 1:
-            return "Rock";
+            return "rock";
         case 2:
-            return "Paper"
+            return "paper"
         case 3:
-            return "Scissors"
+            return "scissors"
     }
 }
